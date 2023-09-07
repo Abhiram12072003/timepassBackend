48801130937999fbf781b6650e0551a826de50ca
abhiram <kilaruabhiram2003@gmail.com>
2023-09-08 01:34:47 +0530

    done

---
+++ server.js
@@ -0,0 +1,20 @@
+const express=require("express");
+const cors=require("cors");
+app=express();
+app.use(express.json());
+
+const mclient=require('mongodb').MongoClient;
+
+app.use(
+    cors({
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST','PUT','PATCH','DELETE'],
+        credentials: true
+    })
+);
+
+mclient.connect('mongodb+srv://abhiram:6309422@cluster0.zgy92ci.mongodb.net/?retryWrites=true&w=majority')
+    .then((dbRef)=>{
+        console.log('DB connection success')
+    })
+    .catch(err=>console.log("database connect error:",err));
